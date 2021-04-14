7130d1424ab2c1a0804230a4e9fcb045bc3159a0
mattheusrml <mattheusrml@gmail.com>
2021-04-13 22:59:31 -0300

    created action get_by_id to User

--- lib/rockelivery/users.ex
+++ lib/rockelivery/users.ex
@@ -1,6 +1,7 @@
 defmodule Rockelivery.Users do
   alias Rockelivery.Users.User
 
-  defdelegate create_user(params), to: User.Create, as: :call
+  defdelegate create(params), to: User.Create, as: :call
+  defdelegate get_by_id(id), to: User.Get, as: :by_id
   
 end
--- lib/rockelivery_web/controllers/users_controller.ex
+++ lib/rockelivery_web/controllers/users_controller.ex
@@ -8,7 +8,7 @@ defmodule RockeliveryWeb.UsersController do
   action_fallback FallbackController
 
   def create(conn, params) do
-    with {:ok, %User{} = user} <- Users.create_user(params) do
+    with {:ok, %User{} = user} <- Users.create(params) do
       conn
       |> put_status(:ok)
       |> render("create.json", user: user)
