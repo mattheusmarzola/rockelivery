c0b98c1cf2ca90b9e51365c3de8dcfdae816133c
mattheusrml <mattheusrml@gmail.com>
2021-04-14 21:47:20 -0300

    create error struct and refatoring error messages

--- lib/rockelivery/users/user/create.ex
+++ lib/rockelivery/users/user/create.ex
@@ -1,5 +1,6 @@
 defmodule Rockelivery.Users.User.Create do
   
+  alias Rockelivery.Error
   alias Rockelivery.Repo
   alias Rockelivery.Users.User
 
@@ -12,6 +13,6 @@ defmodule Rockelivery.Users.User.Create do
 
   defp handle_insert({:ok, %User{}} = result), do: result
   defp handle_insert({:error, result}) do
-    {:error, %{status: :bad_request, result: result}} 
+    {:error, Error.build(:bad_request, result)}
   end  
 end
--- lib/rockelivery/users/user/get.ex
+++ lib/rockelivery/users/user/get.ex
@@ -1,20 +1,21 @@
 defmodule Rockelivery.Users.User.Get do
 
   alias Ecto.UUID
+  alias Rockelivery.Error
   alias Rockelivery.Repo
   alias Rockelivery.Users.User
   
 
   def by_id(id) do
     case UUID.cast(id) do
-      :error -> {:error, %{status: :bad_request, result: "Invalid id format!"}}
+      :error -> {:error, Error.build_user_id_format_error()}
       {:ok, uuid} -> get_by_id(uuid)
     end
   end
 
   defp get_by_id(id) do
     case  Repo.get(User, id) do
-      nil ->  {:error, %{status: :not_found, result: "User not found!"}}
+      nil ->  {:error, Error.build_user_not_found_error()}
       user -> {:ok, user}
     end
   end
