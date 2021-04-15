22949db508e0cf225217a1d8a7d3d1cfd909b781
mattheusrml <mattheusrml@gmail.com>
2021-04-14 23:17:07 -0300

    add Plug.UUIDCheck to valid uuid format, refactoring actions

--- lib/rockelivery/users/user/get.ex
+++ lib/rockelivery/users/user/get.ex
@@ -1,19 +1,11 @@
 defmodule Rockelivery.Users.User.Get do
 
-  alias Ecto.UUID
   alias Rockelivery.Error
   alias Rockelivery.Repo
   alias Rockelivery.Users.User
   
 
   def by_id(id) do
-    case UUID.cast(id) do
-      :error -> {:error, Error.build_user_id_format_error()}
-      {:ok, uuid} -> do_by_id(uuid)
-    end
-  end
-
-  defp do_by_id(id) do
     case  Repo.get(User, id) do
       nil ->  {:error, Error.build_user_not_found_error()}
       user -> {:ok, user}
--- lib/rockelivery/users/user/update.ex
+++ lib/rockelivery/users/user/update.ex
@@ -1,19 +1,11 @@
 defmodule Rockelivery.Users.User.Update do
 
-  alias Ecto.UUID
   alias Rockelivery.Error
   alias Rockelivery.Repo
   alias Rockelivery.Users.User
   
 
   def call(%{"id" => id} = params) do
-    case UUID.cast(id) do
-      :error -> {:error, Error.build_user_id_format_error()}
-      {:ok, _uuid} -> update(params)
-    end
-  end
-
-  defp update(%{"id" => id} = params) do
     case  Repo.get(User, id) do
       nil -> {:error, Error.build_user_not_found_error()}
       user -> do_update(user, params)
