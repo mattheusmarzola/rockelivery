51f8e3bed3e70b0f5b7ffbb0b418c22de7896ced
mattheusrml <mattheusrml@gmail.com>
2021-04-17 18:33:46 -0300

    create items migration and schema

---
+++ lib/rockelivery/items/item.ex
@@ -0,0 +1,29 @@
+defmodule Rockelivery.Items.Item do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  @required_params [:category, :description, :price, :photo]
+  @items_categories [:food, :drink, :desert]
+
+  @derive {Jason.Encoder, only: @required_params ++ [:id]}
+
+  schema "items" do
+    field :category, Ecto.Enum, values: @items_categories
+    field :description, :string
+    field :photo, :string
+    field :price, :decimal
+
+    timestamps() 
+  end
+
+  def changeset(struct \\ %__MODULE__{}, params) do
+    struct
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:description, min: 3)
+    |> validate_number(:price, greater_than: 0)
+  end
+
+end
